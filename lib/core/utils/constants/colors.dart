ba720b1bc3cf146d42377d16fe9abc05f685512e
Ahmd1Khald <ahmed.khaled.sayed2000@gmail.com>
2023-05-09 16:39:01 +0300

    extra colors

--- lib/core/utils/constants/colors.dart
+++ lib/core/utils/constants/colors.dart
@@ -3,6 +3,18 @@ import 'package:hexcolor/hexcolor.dart';
 
 class AppColors {
   static Color backgroundColor = HexColor('#35353D');
+  static Color midBlue = HexColor('#4A86F7');
+  static Color grey = HexColor('#6A7185');
+  static Color midGrey = HexColor('#E9EDF1');
+  static Color lightGrey = HexColor('#F5F7FA');
+  static Color blue = HexColor('#4A86F7');
+  //TO #4A86F7
+  static Color skyBlue = HexColor('#57B6F0');
+  static Color red = HexColor('#D94841');
+  static Color orange = HexColor('#F2A84C');
+  static Color midGreen = HexColor('#83BF6E');
+  static Color green = HexColor('#73BC78');
+  //TO 438A62
 
   static Color? messageColor = Colors.green[300];
 
